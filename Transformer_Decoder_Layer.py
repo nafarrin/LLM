fcb40ebd7b6e3e8936ae9cef83c0b2152941f5da
nafarrin <nafarrin@gmail.com>
2024-05-16 19:13:05 +0200

    Create Transformer_Decoder_Layer.py

---
+++ Transformer_Decoder_Layer.py
@@ -0,0 +1,33 @@
+class TransformerDecoderLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(TransformerDecoderLayer, self).__init__()
+        self.mha1 = MultiHeadAttention(num_heads, d_model)
+        self.mha2 = MultiHeadAttention(num_heads, d_model)
+
+        self.ffn = tf.keras.Sequential([
+            Dense(dff, activation='relu'), 
+            Dense(d_model)
+        ])
+
+        self.layernorm1 = LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = LayerNormalization(epsilon=1e-6)
+        
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+        self.dropout3 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(attn1 + x)
+
+        attn2, attn_weights_block2 = self.mha2(enc_output, enc_output, out1, padding_mask)
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layernorm2(attn2 + out1)
+
+        ffn_output = self.ffn(out2)
+        ffn_output = self.dropout3(ffn_output, training=training)
+        out3 = self.layernorm3(ffn_output + out2)
+
+        return out3, attn_weights_block1, attn_weights_block2
