6ce4b653bfa51074fe0279db7e219f460759b122
nafarrin <nafarrin@gmail.com>
2024-05-16 19:06:25 +0200

    Create Transformer_Encoder_Layer.py

---
+++ Transformer_Encoder_Layer.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow.keras.layers import MultiHeadAttention, LayerNormalization, Dense
+
+class TransformerEncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(TransformerEncoderLayer, self).__init__()
+        self.mha = MultiHeadAttention(num_heads, d_model)
+        self.ffn = tf.keras.Sequential([
+            Dense(dff, activation='relu'), 
+            Dense(d_model)
+        ])
+
+        self.layernorm1 = LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+    
+    def call(self, x, training):
+        attn_output = self.mha(x, x, x)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)
+
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(out1 + ffn_output)
+
+        return out2
